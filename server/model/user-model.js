039d786921a83809d3fbfb6d86d9a35a2166f43c
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-08 22:17:55 +0530

    created user model for the user registration and login form

---
+++ server/model/user-model.js
@@ -0,0 +1,29 @@
+// schema:Defines the structure of the documents within a collection. It Specifies the fields, their types, and any additional constraints or validations.
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: boolean,
+        default: false
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
