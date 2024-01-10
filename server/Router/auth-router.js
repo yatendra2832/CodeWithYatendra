33c697863595456182f22c24bbd338cd409da5db
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-11 00:03:47 +0530

    added zod validaton schema and middleware in the registration form

--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/auth-controller')
+const signupSchema = require('../validators/auth-validator');
+const validate = require('../middleware/validate-middleware');
 
 router.route('/home').get(authController.home);
-router.route('/registration').post(authController.registration);
+router.route('/registration').post(validate(signupSchema), authController.registration);
 router.route('/login').post(authController.login);
 
 module.exports = router;
