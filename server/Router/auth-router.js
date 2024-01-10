92c35f8a415369d21f5ba085c30632fef2ded819
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-11 00:36:11 +0530

    added error middleware to the loginschema and authvalidator

--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/auth-controller')
-const signupSchema = require('../validators/auth-validator');
+const { signupSchema, loginSchema } = require('../validators/auth-validator');
 const validate = require('../middleware/validate-middleware');
 
 router.route('/home').get(authController.home);
 router.route('/registration').post(validate(signupSchema), authController.registration);
-router.route('/login').post(authController.login);
+router.route('/login').post(validate(loginSchema), authController.login);
 
 module.exports = router;
