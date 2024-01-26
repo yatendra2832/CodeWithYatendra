855134d8193f64e6e8658242eb7ca30e448ff347
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-26 10:53:45 +0530

    signup page created

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
 
 const signup = () => {
   return (
-    <div>signup</div>
-  )
-}
+    <div className="row g-0  rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative my-4 mx-4">
+      <div className="col d-none d-lg-block m-4 p-4">
+        <img
+          src="/images/blue_images/register.png"
+          alt="contact image"
+          width={"400"}
+          height={"400"}
+          className=" rounded-circle color-primary shadow-lg"
+        />
+      </div>
+      <div className="col p-4 d-flex flex-column position-static">
+        <div className="container mt-1 ">
+          <h2 className="text-center text-primary fw-bold fs-1">Signup</h2>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Your Username"
+              />
+            </div>
 
-export default signup
+            <div className="mb-4">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="name@example.com"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="phone" className="form-label">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="phone"
+                placeholder="Your Phone Number"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Your Password"
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary w-75 d-block mx-auto">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default signup;
