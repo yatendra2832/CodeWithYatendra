1c3f40ca90b716574a6b9271f0f9c5f0ad496e64
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-27 20:10:18 +0530

    handling login form data and storing in the state variable

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative my-4 mx-4">
       <div className="col d-none d-lg-block m-4 p-4">
@@ -15,16 +35,21 @@ const Login = () => {
       <div className="col p-4 d-flex flex-column position-static">
         <div className="container mt-1 ">
           <h2 className="text-center text-primary fw-bold fs-1">Login</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label htmlFor="email" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
+                name="email"
                 className="form-control"
                 id="email"
                 placeholder="name@example.com"
+                required
+                autoComplete="off"
+                onChange={handleInput}
+                value={user.email}
               />
             </div>
 
@@ -34,9 +59,14 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 className="form-control"
                 id="password"
                 placeholder="Your Password"
+                required
+                autoComplete="off"
+                onChange={handleInput}
+                value={user.password}
               />
             </div>
             <button
