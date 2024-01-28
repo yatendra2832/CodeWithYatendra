e1f8b839c998f497371fc87a3f3b41507e2914dc
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-28 09:24:01 +0530

    added cors policy for the adding frontend to the backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 const connectDb = require('./utils/db')
@@ -12,6 +13,12 @@ const contactRouter = require('./Router/contact-router');
 
 
 // Middlewares
+const corsOptions = {
+    origin: "http://localhost:5173",
+    methods: "GET,POST,DELETE,PUT,PATCH",
+    credentials: true
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/form', applicationRouter);
