2101268ca04d7b49d5ab5d1f90a68cf920988cf0
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-30 10:13:55 +0530

    create Logout page functionality

---
+++ client/src/pages/Logout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../store/auth";
+const Logout = () => {
+  const { LogoutUser } = useAuth();
+
+  useEffect(() => {
+    LogoutUser();
+  }, [LogoutUser]);
+
+  return <Navigate to={"/login"} />;
+};
+
+export default Logout;
