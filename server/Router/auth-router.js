c064673c6c725cbe02b854b44757403dcd3ad93c
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-01-30 18:24:34 +0530

    add the route for the getting the user data

--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authController = require('../controller/auth-controller')
 const { signupSchema, loginSchema } = require('../validators/auth-validator');
 const validate = require('../middleware/validate-middleware');
+const authMiddleware = require('../middleware/auth-middleware')
 
 router.route('/home').get(authController.home);
 router.route('/registration').post(validate(signupSchema), authController.registration);
 router.route('/login').post(validate(loginSchema), authController.login);
 
+router.route('/user').get(authMiddleware, authController.user);
 module.exports = router;
